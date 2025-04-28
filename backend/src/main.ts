60344c188b2e890065ff63726778d86812282917
Will-CJ <w1ll14m285@gmail.com>
2025-04-28 15:07:39 +0700

    add CORS to listen port 4000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Allow CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', // frontend kamu
+    credentials: true, // kalau perlu cookie
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 3000);
 }
