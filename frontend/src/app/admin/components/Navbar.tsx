589dfe066346d8209786aa523bfcc61e27a84c05
Will-CJ <w1ll14m285@gmail.com>
2025-04-28 15:08:54 +0700

    fix: minor change on navbar

--- frontend/src/app/admin/components/Navbar.tsx
+++ frontend/src/app/admin/components/Navbar.tsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       {/* Sidebar */}
       <nav
         id="sidenav-8"
-        className="fixed left-0 top-0 z-[1035] h-full min-h-[100vh] w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800 md:invisible visible"
+        className="fixed left-0 top-0 z-[1035] h-full min-h-[100vh] w-60 -translate-x-full overflow-hidden bg-white shadow-[0_4px_12px_0_rgba(0,0,0,0.07),_0_2px_4px_rgba(0,0,0,0.05)] data-[te-sidenav-hidden='false']:translate-x-0 dark:bg-zinc-800 invisible md:!visible"
         data-te-sidenav-init
         data-te-sidenav-hidden="false"
         data-te-sidenav-position="fixed"
