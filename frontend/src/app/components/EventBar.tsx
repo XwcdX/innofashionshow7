cb007d8ae3940ceb47b7bfda7a5ab482c44bf3b2
ezradesmonds <ezradesmonds@gmail.com>
2025-05-11 10:10:41 +0700

    About us

--- frontend/src/app/components/EventBar.tsx
+++ frontend/src/app/components/EventBar.tsx
@@ -78,7 +78,7 @@ export default function EventBar() {
     
     <section
       id='events'
-      className="min-h-[50vh] text-dark py-16 relative overflow-hidden md:-mb-40 mt-65 sm:mt-0" // allows layers
+      className="min-h-[50vh] text-dark py-16 relative overflow-hidden md:-mb-40 mt-90 sm:mt-0" // allows layers
       style={{
         background: 'transparent',
         scrollSnapAlign: 'start',
